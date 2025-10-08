67bc6ff9d38188de77f99d4d4d2aadf705dd777d
Tolani <omotolaniafolayan85@gmail.com>
2025-10-08 14:09:23 +0100

    all links adjusted

--- keyFeatures.js
+++ keyFeatures.js
@@ -1,43 +1,30 @@
-// import cellTower from './src/assets/cellTower.png'
-import browsers from "./src/assets/browsers.png";
-import chemicalFlask from "./src/assets/chemical_flask.png";
-import cubeFocus from "./src/assets/cubefocus.png";
-import kanban from "./src/assets/kanban.png";
-import usersFour from "./src/assets/users_four.png";
-
 export const keyFeatures = [
   {
-    icon: usersFour,
     feature: "Beginner-Friendly Curriculum",
     description:
       "No prior knowledge required — we start from the very basics of DNA, genes, and genomes before introducing bioinformatics tools.",
   },
   {
-    icon: chemicalFlask,
     feature: "Global + Local Learning Experience",
     description:
       "Join live sessions delivered by international experts, while also participating in community-led watch-parties or online hubs in your region.",
   },
   {
-    icon: usersFour,
     feature: "Hands-On Training",
     description:
       "Apply what you learn immediately through guided mini-projects: from retrieving genome data to basic annotation and visualization.",
   },
   {
-    icon: cubeFocus,
     feature: "Interactive Support",
     description:
       "Engage in live Q&A, moderated discussions, and dedicated WhatsApp groups for continuous guidance and peer learning.",
   },
   {
-    icon: kanban,
     feature: "Certification of Participation",
     description:
       "Receive a globally recognized certificate from Genomac Institute to showcase your learning and boost your CV.",
   },
   {
-    icon: browsers,
     feature: "Career & Research Pathways",
     description:
       "Get early access to mentorship, fellowships, and Train-the-Trainer programs — designed to help you take the next step in bioinformatics.",
