72d45963c27a42cc6dd7fad41ac8967eb266f8b2
Tolani <omotolaniafolayan85@gmail.com>
2025-10-30 19:26:57 +0100

    closed form

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Home from "./Pages/Home.jsx";
 import Form from "./Pages/Form.jsx";
 import { Routes, Route } from "react-router-dom";
 import Auth from "./Pages/Auth.jsx";
+import ClosedForm from "./Pages/ClosedForm.jsx";
 
 const App = () => {
   return (
     <div className="">
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/form" element={<Form />} /> */}
+        <Route path="/form" element={<ClosedForm />} />
         {/* <Route path="/auth" element={<Auth />} /> */}
       </Routes>
     </div>
---
+++ src/Pages/ClosedForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ClosedForm = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen relative">
+      <Link to="/">
+        <button className="border border-[#9D3CA7] rounded-full p-2 px-2.5 md:p-2.5 md:px-4 text-[14px] md:text-[16px] cursor-pointer absolute top-5 left-5  md:top-20 md:left-20">
+          &larr; Back to Home
+        </button>
+      </Link>
+      <h1 className=" text-2xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#511E8C] to-[#9D3CA7]">
+        Registration is now closed.
+      </h1>
+    </div>
+  );
+};
+
+export default ClosedForm;
